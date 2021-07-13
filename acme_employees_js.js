e997c5639b18d5fad12db24c8cb44d52b8111e0b
Andy Gao <andygao1990@gmail.com>
2021-07-13 18:52:53 -0400

    third commit

--- acme_employees_js.js
+++ acme_employees_js.js
@@ -16,7 +16,44 @@ const employees = [
     const stars = new Array(5).fill('*').join('');
     console.log(`${stars} ${text} ${stars}`);
   }
-  
+
+  const findEmployeeByName = (name, employees) => {
+    return employees.find(employee => employee.name === name)
+  }
+
+  const findManagerFor = (employee, employees) => {
+    return employees.find(manager => manager.id === employee.managerId)
+  }
+
+  const findCoworkersFor = (employee, employees) => {
+    return employees.filter(person => person.managerId === employee.managerId && person.name !== employee.name)
+  }
+
+  const findManagementChainForEmployee = (employee, employees) => {
+    if (employee.managerId === undefined) {
+      return []
+    } else {
+      let manager = employees.find(manager => manager.id === employee.managerId)
+      return [...findManagementChainForEmployee(manager, employees), manager]
+    }
+  }
+
+  const findEmployeesFor = (manager, employees) => {
+    return employees.filter(employee => employee.managerId === manager.id)
+  }
+
+  const generateManagementTree = (employees) => {
+    const manager = employees.find(employee => employee.managerId === undefined)
+    const employees1 = findEmployeesFor(manager, employees)
+    manager.reports = employees1
+    for (let i = 0;  i < employees1.length; i++){
+      const employees2 = findEmployeesFor(employees1[i], employees)
+      manager.employees1[i].reports = employees2
+    }
+    return manager
+  }
+
+
   spacer('findEmployeeByName Moe')
   // given a name and array of employees, return employee
   console.log(findEmployeeByName('moe', employees));//{ id: 1, name: 'moe' }
